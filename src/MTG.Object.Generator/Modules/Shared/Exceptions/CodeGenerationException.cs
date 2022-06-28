7003951e23648e63800eb2f2ec22eab59c430891
Mark gottselig <mark.gottselig@gmail.com>
2022-06-27 23:39:22 -0600

    Add serialization to CodeGenerationException

--- src/MTG.Object.Generator/Modules/Shared/Exceptions/CodeGenerationException.cs
+++ src/MTG.Object.Generator/Modules/Shared/Exceptions/CodeGenerationException.cs
@@ -1,6 +1,12 @@
-﻿namespace MTG.Object.Generator.Modules.Shared.Exceptions;
+﻿using System.Runtime.Serialization;
 
+namespace MTG.Object.Generator.Modules.Shared.Exceptions;
+
+[Serializable]
 public class CodeGenerationException : Exception {
     public CodeGenerationException(string message) : base(message) {
+    }
+
+    protected CodeGenerationException(SerializationInfo info, StreamingContext context) : base(info, context) {
     }
 }
