10e8ee0edb0dd2346bebda6d6df8b46da818183a
Mark gottselig <mark.gottselig@gmail.com>
2022-06-27 23:14:20 -0600

    Fix sonarqube code smell

--- src/MTG.Object.Generator/Modules/Shared/Exceptions/CodeGenerationException.cs
+++ src/MTG.Object.Generator/Modules/Shared/Exceptions/CodeGenerationException.cs
@@ -1,6 +1,6 @@
 ﻿namespace MTG.Object.Generator.Modules.Shared.Exceptions;
 
-internal class CodeGenerationException : Exception {
+public class CodeGenerationException : Exception {
     public CodeGenerationException(string message) : base(message) {
     }
 }
