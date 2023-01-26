cf2475e26ac75b8a7a50645682006d80e4fcb436
Mark Gottselig <greven145@users.noreply.github.com>
2023-01-26 04:39:34 +0000

    Automated code generation

--- src/MTG.Objects/Enums/Keywords/KeywordAbilities.cs
+++ src/MTG.Objects/Enums/Keywords/KeywordAbilities.cs
@@ -196,5 +196,6 @@ public sealed class KeywordAbilities : SmartEnum<KeywordAbilities> {
     public static readonly KeywordAbilities Unearth = new(nameof(Unearth), 185);
     public static readonly KeywordAbilities Prototype = new(nameof(Prototype), 186);
     public static readonly KeywordAbilities ForMirrodin! = new(nameof(ForMirrodin!), 187);
+    public static readonly KeywordAbilities Toxic = new(nameof(Toxic), 188);
     public KeywordAbilities(string name, int value) : base(name, value){}
 }
